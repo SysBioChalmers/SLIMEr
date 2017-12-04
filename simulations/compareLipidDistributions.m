f4dec57c6439d5b059268f9dcd251d0ee6b1da86
BenjaSanchez <bensan@chalmers.se>
2017-12-04 11:40:53 +0100

    fix: show abundances as fractions

--- simulations/compareLipidDistributions.m
+++ simulations/compareLipidDistributions.m
@@ -10,7 +10,7 @@ chains = {'C14:0','C16:0','C16:1','C18:0','C18:1'};
 %Compare distributions of chains:
 chains_old = getChainDistribution(model_correctedComp,chains);
 chains_new = getChainDistribution(model_SLIMEr,chains);
-data       = [chains_old chains_new];
+data       = [chains_old/sum(chains_old) chains_new/sum(chains_new)]*100;
 
 %Plot data:
 figure('position', [100,100,900,400])
@@ -28,13 +28,12 @@ end
 text_size = 12;
 set(gca,'XTick',1:5,'XTickLabel',chains)
 set(gca,'FontSize',text_size)
-ylabel('Abundance [g/gDW]','FontSize',text_size);
+ylabel('Abundance [%]','FontSize',text_size);
 legend('boxoff')
 set(gca,'FontSize',text_size)
 set(gca,'XColor','k')
 set(gca,'YColor','k')
 box on
 hold off
-print(gcf,'-depsc')
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
