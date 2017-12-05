18f182a19f53ceae9632985002abdf86ec018ac8
BenjaSanchez <bensan@chalmers.se>
2017-12-05 11:46:10 +0100

    refactor: added generic barPlot function

--- simulations/compareLipidDistributions.m
+++ simulations/compareLipidDistributions.m
@@ -1,8 +1,10 @@
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-% compareLipidDistributions
+% compareLipidDistributions(model_correctedComp,model_SLIMEr)
 %
-% Benjamín J. Sánchez. Last update: 2017-12-04
+% Benjamín J. Sánchez. Last update: 2017-12-05
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+function compareLipidDistributions(model_correctedComp,model_SLIMEr)
 
 %chains:
 chains = {'C14:0','C16:0','C16:1','C18:0','C18:1'};
@@ -11,14 +13,23 @@ chains = {'C14:0','C16:0','C16:1','C18:0','C18:1'};
 chains_old = getChainDistribution(model_correctedComp,chains);
 chains_new = getChainDistribution(model_SLIMEr,chains);
 data       = [chains_old/sum(chains_old) chains_new/sum(chains_new)]*100;
+color      = [1  1  0      %Yellow
+              1  0  0];    %Red
+barPlot(data,chains,color)
+legend('Yeast7 - correct lipid composition','Yeast7 - correct lipid+chain composition','Location','northwest')
+legend('boxoff')
+
+
+end
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+function barPlot(data,names,color)
 
 %Plot data:
 figure('position', [100,100,900,400])
 hold on
 b = bar(data,'BarWidth',1);
-color = [1  1  0      %Yellow
-         1  0  0];    %Red
-legend(b,'Yeast7 - correct lipid composition','Yeast7 - correct lipid+chain composition','Location','northwest')
 
 for i = 1:length(b)
     set(b(i),'FaceColor',color(i,:));
@@ -26,14 +37,15 @@ end
 
 %Various options:
 text_size = 12;
-set(gca,'XTick',1:5,'XTickLabel',chains)
+set(gca,'XTick',1:length(names),'XTickLabel',names)
 set(gca,'FontSize',text_size)
 ylabel('Abundance [%]','FontSize',text_size);
-legend('boxoff')
 set(gca,'FontSize',text_size)
 set(gca,'XColor','k')
 set(gca,'YColor','k')
 box on
 hold off
+
+end
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
