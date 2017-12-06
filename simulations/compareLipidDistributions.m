568800e73fa895fe0ee59ea45e8e0935efc04f9b
BenjaSanchez <bensan@chalmers.se>
2017-12-06 11:18:39 +0100

    feat: created plot of lipid distributions

--- simulations/compareLipidDistributions.m
+++ simulations/compareLipidDistributions.m
@@ -9,10 +9,10 @@ function compareLipidDistributions(model_correctedComp,model_SLIMEr)
 %1. Exp data: lipid classes
 cd ../data
 fid = fopen('lipid_data.csv');
-lipidData = textscan(fid,'%s %s %s %f32','Delimiter',',','HeaderLines',1);
-lipids    = lipidData{1}([1,3:end]);  %Take out ergosterol
-lipidIDs  = lipidData{3}([1,3:end]);  %Take out ergosterol
-data      = lipidData{4}([1,3:end]);  %Take out ergosterol
+lipidData  = textscan(fid,'%s %s %s %f32','Delimiter',',','HeaderLines',1);
+lipids     = lipidData{1}([1,3:end]);  %Take out ergosterol
+lipidNames = lipidData{2}([1,3:end]);  %Take out ergosterol
+data       = lipidData{4}([1,3:end]);  %Take out ergosterol
 fclose(fid);
 cd ../simulations
 color = [0  1  0];    %Green
@@ -41,6 +41,16 @@ barPlot(data,chains,'[%]',color,60)
 legend('Yeast7 - correct lipid composition','Yeast7 - correct lipid+chain composition','Location','northwest')
 legend('boxoff')
 
+%4. Compare distribution of chains in lipids:
+data  = getLipidDistribution(model_SLIMEr,lipidNames,chains);
+color = [0    1    0
+         0.2  0.7  0
+         0.5  0.5  0
+         0.8  0.3  0
+         1    0    0];
+barPlot(data,lipids,'[g/gDW]',color,0.005)
+legend(chains,'Location','northwest')
+legend('boxoff')
 
 end
 
---
+++ simulations/getLipidDistribution.m
@@ -0,0 +1,51 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% data = getLipidDistribution(model,lipidIDs,chains)
+%
+% Benjamín J. Sánchez. Last update: 2017-12-06
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+function composition = getLipidDistribution(model,lipidNames,chains)
+
+%Simulate model:
+sol = optimizeCbModel(model);
+
+%Find growth:
+Xpos = strcmp(model.rxnNames,'growth');
+mu   = sol.x(Xpos);
+
+for i = 1:length(chains)
+    chains{i} = ['C' chains{i} ' chain [cytoplasm]'];
+end
+
+composition = zeros(length(lipidNames),length(chains));
+
+%Go through all SLIME rxns to find abundances:
+SLIMEpos = find(~cellfun(@isempty,strfind(model.rxnNames,'SLIME rxn')));
+for i = 1:length(SLIMEpos)
+    %Find flux and all metabolites produced in each SLIME rxn:
+    flux      = sol.x(SLIMEpos(i));
+    metPos    = model.S(:,SLIMEpos(i)) > 0;
+    metNames  = model.metNames(metPos);
+    metStoich = model.S(metPos,SLIMEpos(i));
+    
+    %Find lipid species:
+    pos_i = [];
+    for j = 1:length(lipidNames)
+        lipidPos = ~cellfun(@isempty,strfind(model.metNames(metPos),lipidNames{j}));
+        if sum(lipidPos) > 0
+            pos_i = j;
+        end
+    end
+    
+    %Find chains produced:
+    for j = 1:length(chains)
+        chainPos = strcmp(metNames,chains{j});
+        if sum(chainPos) > 0
+            composition(pos_i,j) = composition(pos_i,j) + flux*metStoich(chainPos)/mu;
+        end
+    end
+end
+
+end
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
