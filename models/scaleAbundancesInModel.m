fb981ffee79241ab2f9e4b642bb24767aceee004
BenjaSanchez <bensan@chalmers.se>
2017-12-07 16:25:44 +0100

    feat: finds optimality range for scaling
    Method now finds all possible scalings that results in an optimally grown simulation with no unused chains or backbones. Assigns to the output model the highest value found

--- models/scaleAbundancesInModel.m
+++ models/scaleAbundancesInModel.m
@@ -1,15 +1,32 @@
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % model = scaleAbundancesInModel(model)
 %
-% Benjamín J. Sánchez. Last update: 2017-12-04
+% Benjamín J. Sánchez. Last update: 2017-12-07
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 function model = scaleAbundancesInModel(model)
 
-k0 = 1;
-k  = fminsearch(@unusedLipid,k0);
-disp(['Scaled chain data in model: k = ' num2str(k)])
-model = adjustModel(model,k);
+%Find optimal scaling factor:
+k0       = 1;
+kOpt     = fminsearch(@unusedLipid,k0);
+modelOpt = adjustModel(model,kOpt);
+
+%Optimize model:
+cd ../simulations
+solOpt = simulateGrowth(modelOpt);
+muOpt  = solOpt.f;
+cd ../models
+save('kOpt.mat','kOpt','muOpt')
+
+%Find optimality range:
+krange(1) = fminsearch(@(x) +minScaling(x),kOpt);
+krange(2) = fminsearch(@(x) -minScaling(x),kOpt);
+disp(['Optimality range: k = [ ' num2str(krange(1)) ' , ' num2str(krange(2)) ' ]'])
+
+%Scale with the maximum of the range:
+model = adjustModel(model,krange(2));
+disp(['Scaled chain data in model: k = ' num2str(krange(2))])
+delete('kOpt.mat')
 
 end
 
@@ -33,6 +50,37 @@ exchange_backs = sol.x(strcmp(model.rxnNames,'lipid - backbones exchange'));
 exchange       = exchange_tails + exchange_backs;
 
 disp(['Scaling abundance data: k = ' num2str(k) ' -> exchange = ' num2str(exchange)])
+
+end
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+function k = minScaling(k)
+
+%Load model and adjust stoich coeffs of the tail pseudo-rxn:
+model = load('yeast_7.8_SLIMEr.mat');
+model = model.model_SLIMEr;
+model = adjustModel(model,k);
+
+%block exchange of tails and backbones:
+posT = strcmp(model.rxnNames,'lipid - tails exchange');
+posB = strcmp(model.rxnNames,'lipid - backbones exchange');
+model = changeRxnBounds(model,model.rxns(posT),0,'b');
+model = changeRxnBounds(model,model.rxns(posB),0,'b');
+
+%Optimize model:
+cd ../simulations
+sol = simulateGrowth(model);
+cd ../models
+
+disp(['Finding scaling range: k = ' num2str(k) ' -> mu = ' num2str(sol.f)])
+
+%Any simulation that results in impaired growth (>1%) returns the original value:
+kOpt  = load('kOpt.mat');
+muOpt = kOpt.muOpt;
+if abs(sol.f - muOpt)/muOpt > 0.01
+    k = kOpt.kOpt;
+end
 
 end
 
