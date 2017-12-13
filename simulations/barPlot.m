663bd543a3916a469111c54f1e617856754324ca
BenjaSanchez <bensan@chalmers.se>
2017-12-13 12:23:58 +0100

    feat: included variability plots
    + margin fix in existent figures

--- simulations/barPlot.m
+++ simulations/barPlot.m
@@ -1,18 +1,36 @@
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-% barPlot(data,names,units,color,ymax,xlength)
+% b = barPlot(data,names,units,color,ymax,xlength)
 %
 % Benjamín J. Sánchez. Last update: 2017-12-13
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-function barPlot(data,names,units,color,ymax,xlength)
+function b = barPlot(data,names,units,color,ymax,xlength)
 
-%Plot data:
 figure('position', [100,100,xlength,400])
 hold on
-b = bar(data,'BarWidth',1);
 
-for i = 1:length(b)
-    set(b(i),'FaceColor',color(i,:));
+if isfield(data,'var')
+    %Variability plot:
+    [M,N] = size(data.comp);
+    for i = 1:M
+        for j = 1:N
+            comp  = data.comp(i,j);
+            min   = data.var.min(i,j);
+            max   = data.var.max(i,j);
+            left  = i - 0.5 + (j-1)/N;
+            right = i - 0.5 + j/N;
+            b(j) = patch([left,right,right,left],[min,min,max,max],color(j,:));
+            plot([left,right],[comp,comp],'-k','LineWidth',1.5)
+        end
+        plot([right,right],[0,ymax],'--k')
+    end
+else
+    %Simple barplot:
+    [M,N] = size(data);
+    b = bar(data,'BarWidth',1);
+    for i = 1:N
+        set(b(i),'FaceColor',color(i,:));
+    end
 end
 
 %Various options:
@@ -20,6 +38,7 @@ text_size = 12;
 set(gca,'XTick',1:length(names),'XTickLabel',names)
 set(gca,'FontSize',text_size)
 ylabel(['Abundance ' units],'FontSize',text_size);
+xlim([0.5,M+0.5])
 ylim([0 ymax])
 set(gca,'FontSize',text_size)
 set(gca,'XColor','k')
--- simulations/compareLipidDistributions.m
+++ simulations/compareLipidDistributions.m
@@ -14,7 +14,7 @@ data       = lipidData{4}([1,3:end])*1000;  %mg/gDW
 fclose(fid);
 cd ../simulations
 color = [0  1  0];    %Green
-barPlot(data,lipids,'[mg/gDW]',color,15,600)
+barPlot(data,lipids,'[mg/gDW]',color,15,600);
 
 %2. Exp data: chains
 cd ../data
@@ -27,7 +27,7 @@ data      = chainData{3}(1:end-2)*1000;	%mg/gDW
 fclose(fid);
 cd ../simulations
 color = [1  0  0];    %Red
-barPlot(data,chains,'[mg/gDW]',color,25,500)
+barPlot(data,chains,'[mg/gDW]',color,25,500);
 
 %3. Compare distributions of chains:
 old       = getLipidDistribution(model_correctedComp,lipidNames,chains);
@@ -37,18 +37,33 @@ newChains = sum(new.comp)';
 data      = [oldChains/sum(oldChains) newChains/sum(newChains)]*100;
 color     = [1  1  0      %Yellow
              1  0  0];    %Red
-barPlot(data,chains,'[%]',color,60,900)
+barPlot(data,chains,'[%]',color,60,900);
 legend('Yeast7 - correct lipid composition','Yeast7 - correct lipid+chain composition','Location','northwest')
 legend('boxoff')
 
-%4. Compare distribution of chains in lipids:
+%4. Compare distribution of chains in lipids - old model:
 color = [0    1    0
          0.2  0.7  0
          0.5  0.5  0
          0.8  0.3  0
          1    0    0];
-barPlot(new.comp,lipids,'[mg/gDW]',color,10,900)
+barPlot(old.comp,lipids,'[mg/gDW]',color,20,900);
 legend(chains,'Location','northwest')
+legend('boxoff')
+
+%5. Compare variability of chains in lipids - old model:
+b = barPlot(old,lipids,'[mg/gDW]',color,20,900);
+legend(b,chains,'Location','northwest')
+legend('boxoff')
+
+%6. Compare distribution of chains in lipids - new model:
+barPlot(new.comp,lipids,'[mg/gDW]',color,20,900);
+legend(chains,'Location','northwest')
+legend('boxoff')
+
+%7. Compare variability of chains in lipids - new model:
+b = barPlot(new,lipids,'[mg/gDW]',color,20,900);
+legend(b,chains,'Location','northwest')
 legend('boxoff')
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
