6108c327bc432315e31a3633a2cbf2f24d518497
BenjaSanchez <bensan@chalmers.se>
2018-03-23 17:53:17 +0100

    feat: create models for Ejsing data

--- models/createNewModels.m
+++ models/createNewModels.m
@@ -12,14 +12,23 @@ addpath('../data')
 model = load('yeast_7.8.mat');
 model = model.model;
 
-%Create 2 models for each of the 10 conditions:
+%Create 2 models for each of the 10 conditions in the stress dataset:
 model_corrComp = cell(1,10);
-model_SLIMEr        = cell(1,10);
-GAMpol              = zeros(1,10);
-k                   = zeros(1,10);
+model_SLIMEr   = cell(1,10);
+GAMpol         = zeros(1,10);
+k              = zeros(1,10);
 for i = 1:10
     data = readLahtveeData(i);
     [model_corrComp{i},model_SLIMEr{i},k(i),GAMpol(i)] = modelsFromData(model,data);
+end
+
+%Create 2 model for each of the validation studies:
+model_corrComp_val = cell(1,8);
+model_SLIMEr_val   = cell(1,8);
+for i = 1:8
+    data = readEjsingData(i);
+    data = convertEjsingData(data,model,true);
+    [model_corrComp_val{i},model_SLIMEr_val{i},~,~] = modelsFromData(model,data);
 end
 
 rmpath('../simulations')
