bfb82ee04541626a195dc320facee28962ac4ce8
BenjaSanchez <bensan@chalmers.se>
2018-04-02 11:24:32 +0200

    feat: random sampling for both models
    + calculation of model error

---
+++ simulations/lipidRandomSampling.m
@@ -0,0 +1,43 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% abundance = lipidRandomSampling(model,data,Nsim)
+%
+% Benjamín J. Sánchez. Last update: 2018-03-31
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+function abundance = lipidRandomSampling(model,data,Nsim)
+
+%Simulate a flux distribution with the corresponding model:
+[sol,model] = simulateGrowth(model,data.fluxData);
+posX          = strcmp(model.rxnNames,'growth');
+muS           = sol.x(posX);
+
+%Get a number of simulations from random sampling:
+model_r = ravenCobraWrapper(model);
+disp('Initializing random sampling...')
+samples = randomSampling(model_r,Nsim);
+
+%Find matching positions for each species and compute predicted abundance:
+abundance = zeros(length(data.metNames),Nsim);
+isSLIME   = contains(model.rxnNames,'SLIME rxn');
+for j = 1:length(data.metNames)
+    pos = matchToModel(model,data.metNames{j});
+    if sum(pos) > 0
+        %Get MW for each met:
+        MWs = getMWfromFormula(model.metFormulas(pos));     %g/mmol
+        
+        %Get fluxes in which each met gets consumed for the SLIME rxn:
+        isSub   = model.S(pos,:) < 0;
+        fluxPos = isSub.*isSLIME';
+        fluxes  = fluxPos*samples;
+        
+        %Compute abundance predicted by model:
+        abundance(j,:) = sum(fluxes.*MWs,1)/muS*1000;	%mg/gDW
+    end
+end
+
+%Compute error:
+
+
+end
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
--- simulations/lipidValidation.m
+++ simulations/lipidValidation.m
@@ -1,7 +1,7 @@
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % lipidValidation
 %
-% Benjamín J. Sánchez. Last update: 2018-03-26
+% Benjamín J. Sánchez. Last update: 2018-04-02
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 rng default
@@ -9,53 +9,33 @@ close all
 addpath('../data')
 addpath('../models')
 
+errors = zeros(length(model_corrComp_val),2);
 for i = 1%:length(model_corrComp_val)
     %Read experimental data for each condition:
     data = readEjsingData(i);
-    data = convertEjsingData(data,model_SLIMEr_val{i},false);
-    metNames      = data.metNames(1:end-1);         %filter out ergosterol
-    abundance_exp = data.abundance(1:end-1)*1000;   %mg/gDW
-    std           = data.std(1:end-1)*1000;         %mg/gDW
+    data = convertEjsingData(data,model,false);
+    data.metNames  = data.metNames(1:end-1);         %filter out ergosterol
+    data.abundance = data.abundance(1:end-1)*1000;   %mg/gDW
+    data.std       = data.std(1:end-1)*1000;         %mg/gDW
     
-    %Simulate a flux distribution with the corresponding model:
-    [sol,model] = simulateGrowth(model_SLIMEr_val{i},data.fluxData);
-    posX        = strcmp(model.rxnNames,'growth');
-    mu          = sol.x(posX);
+    Nsim = 10000;
+    abundance_modC = lipidRandomSampling(model_corrComp_val{i},data,Nsim);
+    abundance_modS = lipidRandomSampling(model_SLIMEr_val{i},data,Nsim);
     
-    %Get a number of simulations from random sampling:
-    Nsim    = 10000;
-    model_r = ravenCobraWrapper(model);
-    disp('Initializing random sampling...')
-    samples = randomSampling(model_r,Nsim);
-    
-    %Find matching positions for each species and compute predicted abundance:
-    abundance_mod = zeros(length(abundance_exp),Nsim);
-    isSLIME       = contains(model.rxnNames,'SLIME rxn');
-    for j = 1:length(metNames)
-        pos = matchToModel(model,metNames{j});
-        if sum(pos) > 0
-            %Get MW for each met:
-            MWs = getMWfromFormula(model.metFormulas(pos));     %g/mmol
-            
-            %Get fluxes in which each met gets consumed for the SLIME rxn:
-            isSub   = model.S(pos,:) < 0;
-            fluxPos = isSub.*isSLIME';
-            fluxes  = fluxPos*samples;
-            
-            %Compute abundance predicted by model:
-            abundance_mod(j,:) = sum(fluxes.*MWs,1)/mu*1000;	%mg/gDW
-        end
-    end
+    %Averages & errors:
+    means       = [mean(abundance_modC,2) mean(abundance_modS,2)];
+    diffs       = means - ones(size(means)).*data.abundance;
+    errors(i,:) = sum(abs(diffs),1)/length(diffs);
     
     %Reshape model results:
-    x = repmat((1:length(metNames))',Nsim,1);
-    y = reshape(abundance_mod,[length(metNames)*Nsim,1]);
+    x = repmat((1:length(data.metNames))',Nsim,1);
+    y = reshape(abundance_modS,[length(data.metNames)*Nsim,1]);
     
     %Plot data:
     ymax = ceil(max(y)/5)*5;
-    barPlot(abundance_exp,metNames,'[mg/gDW]','r',ymax,1400,std);
+    barPlot(data.abundance,data.metNames,'[mg/gDW]','r',ymax,1400,data.std);
     hold on
-    scatter(x,y,10,'b','MarkerEdgeAlpha',.05)
+    scatter(x,y,10,'b','MarkerEdgeAlpha',.02)
     hold off
 end
 rmpath('../data')
